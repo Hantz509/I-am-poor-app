764956447966e94c70507d4271698acbd2399e5c
Hantz509 <guerrierhantz58@gmail.com>
2024-07-09 18:53:43 +0800

    first commit

---
+++ I Am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am poor
+//
+//  Created by GUERRIER Hantz Brunet on 2024/7/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
